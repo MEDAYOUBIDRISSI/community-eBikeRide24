9ea0d0fe64992047397ff20d68a2f98fe018e435
med ayoub <medayoub.idrissi@gmail.com>
2021-07-30 22:51:55 +0100

    ReadMoreInPosteDetail

--- src/app/@theme/layouts/one-column/one-column.layout.ts
+++ src/app/@theme/layouts/one-column/one-column.layout.ts
@@ -9,6 +9,10 @@ import { Component } from '@angular/core';
         <ngx-header></ngx-header>
       </nb-layout-header>
 
+      <nb-sidebar class="menu-sidebar" tag="menu-sidebar " responsive>
+        <ng-content select="nb-menu"></ng-content>
+      </nb-sidebar>
+      
       <nb-layout-column>
         <ng-content select="router-outlet"></ng-content>
       </nb-layout-column>
@@ -20,7 +24,5 @@ import { Component } from '@angular/core';
   `,
 })
 
-// <nb-sidebar class="menu-sidebar" tag="menu-sidebar " responsive>
-//         <ng-content select="nb-menu"></ng-content>
-//       </nb-sidebar>
+
 export class OneColumnLayoutComponent {}
